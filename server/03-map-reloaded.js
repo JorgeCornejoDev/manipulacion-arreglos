f79e7d9a84116b33e3e4d7812b552da600f2bd6f
Jorge Cornejo Dev <imjorge.dev@gmail.com>
2022-06-11 18:29:49 -0500

    Map Reloaded

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,34 @@
+const orders = [
+    { 
+        customerName: 'Nicolas ',
+        total: 60,
+        delivered: true
+    },
+    {
+        customerName: 'Zulema',
+        total: 120,
+        delivered: false
+    },
+    {
+        customerName: 'Santiago',
+        total: 180,
+        delivered: true
+    },
+    {
+        customerName: 'Valentina',
+        total: 240,
+        delivered: true
+    }
+];
+
+const rta = orders.map( item => item.total );
+const rta2 = orders.map( item => {
+    return {
+        ...item,
+        tax: .19,
+    }
+});
+
+console.log( 'orders - ',orders );
+console.log( 'rta', rta2 );
+
